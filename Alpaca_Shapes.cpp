946c4068303b0857acfe768657febb7fb338fa5f
Daniel Byers <danielbyers0@gmail.com>
2023-02-20 17:47:36 -0500

    Alpaca_Shapes DMOJ

---
+++ Alpaca_Shapes.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+
+int main()
+{
+    int S, R;
+    scanf("%d", &S);
+    scanf("%d", &R);
+    
+    int A_square = S*S;
+    int A_circle = 3.14 * (R*R);
+
+    if (A_square > A_circle)
+    {
+        printf("SQUARE");
+    }
+    else
+    {
+        printf("CIRCLE");
+    }
+}
